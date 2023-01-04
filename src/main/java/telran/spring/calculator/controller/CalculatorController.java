5b5d3ec59a69291aeac66ea3834ab68fd49868e3
pichu <pichurinaa@gmail.com>
2023-01-04 20:07:34 +0200

    HW-54

--- src/main/java/telran/spring/calculator/controller/CalculatorController.java
+++ src/main/java/telran/spring/calculator/controller/CalculatorController.java
@@ -1,12 +1,29 @@
 package telran.spring.calculator.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import telran.spring.calculator.dto.OperationData;
+import telran.spring.calculator.service.Operation;
+
+import java.util.Map;
+import java.util.Set;
 
 @RestController
 @RequestMapping("calculator")
 public class CalculatorController {
-//POST GET
+    Map<String, Operation> operations;
 
+    public CalculatorController(Map<String, Operation> operations) {
+        this.operations = operations;
+    }
 
+    @PostMapping
+    String calculate(@RequestBody OperationData operationData) {
+        Operation operation = operations.get(operationData.type);
+        return operation != null ? operation.perform(operationData) : "Wrong type " + operationData.type;
+    }
+
+    @GetMapping
+    Set<String> getTypes() {
+        return operations.keySet();
+    }
 }
