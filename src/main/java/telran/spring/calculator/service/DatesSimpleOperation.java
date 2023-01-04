27d027001e8852556bb3cbf339008442d92cdaa0
pichu <pichurinaa@gmail.com>
2023-01-04 20:43:59 +0200

    HW-54

--- src/main/java/telran/spring/calculator/service/DatesSimpleOperation.java
+++ src/main/java/telran/spring/calculator/service/DatesSimpleOperation.java
@@ -2,13 +2,30 @@ package telran.spring.calculator.service;
 
 
 import org.springframework.stereotype.Service;
+import telran.spring.calculator.dto.DateDaysOperationData;
 import telran.spring.calculator.dto.OperationData;
+
+import java.time.LocalDate;
 
 @Service("dates simple operation")
 public class DatesSimpleOperation implements Operation {
 
     @Override
     public String execute(OperationData operationData) {
-        return null;
+        DateDaysOperationData data = (DateDaysOperationData) operationData;
+        return String.format("%s",
+                getResult(data, "after") +
+                        getResult(data, "before"));
+    }
+
+    private String getResult(DateDaysOperationData data, String plusMinus) {
+        return String.format("The date %s %s from %s is %s", plusMinus, data.days, data.date,
+                calculate(data, plusMinus));
+    }
+
+    private LocalDate calculate(DateDaysOperationData data, String plusMinus) {
+        return plusMinus.equals("after") ?
+                LocalDate.parse(data.date).plusDays(data.days) :
+                LocalDate.parse(data.date).minusDays(data.days);
     }
 }
