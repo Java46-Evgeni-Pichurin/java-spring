f1fd0806e6875fa9fecd6eacdcd7b84a49a45963
pichu <pichurinaa@gmail.com>
2023-01-04 20:31:39 +0200

    HW-54

--- src/main/java/telran/spring/calculator/service/DatesBetweenOperation.java
+++ src/main/java/telran/spring/calculator/service/DatesBetweenOperation.java
@@ -1,13 +1,19 @@
 package telran.spring.calculator.service;
 
 import org.springframework.stereotype.Service;
+import telran.spring.calculator.dto.ArithmeticOperationData;
+import telran.spring.calculator.dto.DatesOperationData;
 import telran.spring.calculator.dto.OperationData;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 @Service("dates between operation")
 public class DatesBetweenOperation implements Operation {
 
     @Override
     public String execute(OperationData operationData) {
-        return null;
+        DatesOperationData data = (DatesOperationData) operationData;
+        return String.valueOf(ChronoUnit.DAYS.between(LocalDate.parse(data.dateFrom), LocalDate.parse(data.dateTo)));
     }
 }
