df4e01f19f1dc91a9c760024498ad7c4a9f4e500
Evgeni Pichurin <pichurinaa@gmail.com>
2023-01-17 10:54:54 +0200

    HW-57

--- src/main/java/telran/spring/calculator/controller/CalculatorController.java
+++ src/main/java/telran/spring/calculator/controller/CalculatorController.java
@@ -2,56 +2,54 @@ package telran.spring.calculator.controller;
 
 
 import java.util.*;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import jakarta.annotation.*;
-import jakarta.validation.Valid;
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-
+import jakarta.annotation.PostConstruct;
+import jakarta.validation.Valid;
+import org.springframework.web.server.ResponseStatusException;
 import telran.spring.calculator.dto.OperationData;
 import telran.spring.calculator.service.Operation;
-
 
 @RestController
 @RequestMapping("calculator")
 public class CalculatorController {
-    static Logger LOG = LoggerFactory.getLogger(Operation.class);
-    @Value("${app.message.wrong.cast: Wrong casting }")
-    String wrongTypeMessage;
-    Map<String, Operation> operationServices;
-    List<Operation> operationsList;
+	static Logger LOG = LoggerFactory.getLogger(CalculatorController.class);
+	List<Operation> operations;
+	Map<String, Operation> operationServices;
+	@Value("${app.message.wrong.operation.name}")
+	String wrongOperationMessage;
 
-    public CalculatorController(List<Operation> operationsList) {
-        this.operationsList = operationsList;
-    }
-
-    @PostMapping
-    String getOperationResult(@RequestBody @Valid OperationData data) {
-        LOG.debug("received request for operation: {}", data.operationName);
-        Operation operationService = operationServices.get(data.operationName);
-        return operationService != null ? operationService.execute(data) :
-                String.format("%s Should be one of the following %s", wrongTypeMessage, operationServices.keySet());
-    }
-
-    @GetMapping
-    Set<String> getAllOperationNames() {
-        return operationServices.keySet();
-    }
-
-    @PostConstruct
-    void convertToOperationNamesMap() {
-        operationServices = operationsList.stream().collect(Collectors.toMap(Operation::getServiceName, Function.identity()));
-        LOG.info("application context is created with types {}", operationServices.keySet());
-    }
-
-    @PreDestroy
-    void shutdown() {
-        System.out.println("Server closed by graceful shutdown");
-        LOG.info("shutdown performed");
-    }
+	public CalculatorController(List<Operation> operations) {
+		this.operations = operations;
+	}
+	@PostMapping
+	String getOperationResult(@RequestBody @Valid OperationData data) {
+		LOG.debug("operation request with type: {}, additional data: {},"
+				+ " class of Operation data is {}",
+				data.operationName, data.additionalData, data.getClass().getSimpleName());
+		Operation operationService = operationServices.get(data.operationName);
+		if (operationService == null) {
+			LOG.error("operation {} is not implemented", data.operationName);
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, wrongOperationMessage);
+		}
+		return operationService.execute(data);
+	}
+	@GetMapping
+	Set<String> getAllOperationNames() {
+		LOG.debug("request for getting operation names");
+		return operationServices.keySet();
+	}
+	@PostConstruct
+	void createMapOperationsServices() {
+		operationServices = operations.stream()
+				.collect(Collectors.toMap(Operation::getOperationName, service -> service));
+		LOG.info("the operation names are {}", operationServices.keySet());
+	}
+	
 
 }
