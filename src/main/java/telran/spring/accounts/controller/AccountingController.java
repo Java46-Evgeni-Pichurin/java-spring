496fba1eb6d5fc6364a3766fa76e81c10e4b0192
pichu <pichurinaa@gmail.com>
2023-01-19 19:26:25 +0200

    HW-58

--- src/main/java/telran/spring/accounts/controller/AccountingController.java
+++ src/main/java/telran/spring/accounts/controller/AccountingController.java
@@ -2,8 +2,13 @@ package telran.spring.accounts.controller;
 
 import jakarta.annotation.*;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import org.springframework.web.server.ResponseStatusException;
 import telran.spring.accounts.dto.Account;
 import telran.spring.accounts.service.AccountingManager;
 
@@ -12,6 +17,11 @@ import java.util.Map;
 @RestController
 @RequestMapping("accounts")
 public class AccountingController {
+
+    Logger LOG = LoggerFactory.getLogger(AccountingController.class);
+
+    @Value("${app.message.wrong.operation}")
+    String wrongOperationMessage;
     AccountingManager accountingManager;
 
     public AccountingController(AccountingManager accountingManager) {
@@ -20,36 +30,57 @@ public class AccountingController {
 
     @PostMapping
     String addUser(@RequestBody @Valid Account account) {
-        return accountingManager.add(account);
+        if (!accountingManager.add(account)) {
+            LOG.error(wrongOperationMessage + " " + account.username);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    String.format("User with name %s already exists", account.username));
+        }
+        LOG.debug("POST request for adding user with name {}", account.username);
+        return String.format("user %s has been added", account.username);
     }
 
     @PutMapping
     String updateUser(@RequestBody @Valid Account account) {
-        return accountingManager.update(account);
+        if (!accountingManager.update(account)) {
+            LOG.error(wrongOperationMessage + " " + account.username);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    String.format("User with name %s doesn't exist", account.username));
+        }
+        LOG.debug("PUT request for updating user with name {}", account.username);
+        return String.format("user %s has been updated", account.username);
     }
 
     @DeleteMapping("/{username}")
     String deleteUser(@PathVariable("username") String username) {
-        return accountingManager.delete(username);
+        if (accountingManager.delete(username)) {
+            LOG.error(wrongOperationMessage + " " + username);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    String.format("User with name %s doesn't exist", username));
+        }
+        LOG.debug("DELETE request for removing user with name {}", username);
+        return String.format("user %s has been deleted", username);
     }
 
     @GetMapping("/{username}")
     boolean userExists(@PathVariable("username") String username) {
+        LOG.debug("GET request for getting user with name {}", username);
         return accountingManager.exists(username);
     }
 
     @GetMapping("/all")
     Map<String, Account> allUsers() {
+        LOG.debug("GET request for getting all users");
         return accountingManager.getAll();
     }
 
     @PostConstruct
     void restoreAccounts() {
-        accountingManager.restore();
+        LOG.debug("Current number of users: {}", accountingManager.restore("accountData"));
     }
 
     @PreDestroy
     void saveAccounts() {
-        accountingManager.save();
+        accountingManager.save("accountData");
+        LOG.info("Account data file has been updated");
     }
 }
