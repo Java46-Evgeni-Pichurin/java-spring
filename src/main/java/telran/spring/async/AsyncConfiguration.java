0e73f3f3a021d3db88704bb40cf7d224783935ae
pichu <pichurinaa@gmail.com>
2023-01-29 22:12:02 +0200

    HW-60

--- src/main/java/telran/spring/async/AsyncConfiguration.java
+++ src/main/java/telran/spring/async/AsyncConfiguration.java
@@ -21,7 +21,7 @@ public class AsyncConfiguration {
         executor.setCorePoolSize(2);
         executor.setMaxPoolSize(2);
         executor.setQueueCapacity(100);
-        executor.setThreadNamePrefix("CarThread-");
+        executor.setThreadNamePrefix("EmployeeThread-");
         executor.initialize();
         return executor;
     }
